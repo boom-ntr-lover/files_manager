dcbf39a1f114627dc3aaad788387017c93810426
wangzixiao <wangzixiao@ka60om.com>
2023-10-31 21:12:09 +0800

    去除测试代码，准备写界面

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
-    <TestToMain></TestToMain>
+    <h1>Hello World!</h1>
 </template>
 
 <script>
-import TestToMain from './components/TestToMain.vue'
 
 export default {
     name: 'App',
     components:
         {
-            // HelloWorld
-            TestToMain,
         }
 }
 </script>
-
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
