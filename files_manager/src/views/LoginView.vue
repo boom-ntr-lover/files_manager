cf85e65664adba45de69aac7c9e2b925b0c9d83c
wangzixiao <wangzixiao@ka60om.com>
2023-11-01 22:42:07 +0800

    update loading

--- files_manager/src/views/LoginView.vue
+++ files_manager/src/views/LoginView.vue
@@ -1,17 +1,8 @@
 <template>
     <div class="text-center">
 
-        <v-btn
-            :disabled="dialog"
-            :loading="dialog"
-            color="purple-darken-2"
-            @click="dialog = true"
-        >
-            Start loading
-        </v-btn>
-
         <v-dialog
-            v-model="dialog"
+            v-model="waiting"
             :scrim="false"
             persistent
             width="auto"
@@ -20,7 +11,7 @@
                 color="primary"
             >
                 <v-card-text>
-                    Please stand by
+                    Loading
                     <v-progress-linear
                         indeterminate
                         color="white"
@@ -38,32 +29,28 @@ export default {
     data()
     {
         return {
-            dialog: false,
+            waiting: true,
         }
     },
 
-    watch: {
-        dialog(val)
-        {
-            if (!val) return
-
-            setTimeout(() => (this.dialog = false), 4000)
-        },
-    },
+    watch: { },
 
     created()
     {
-        console.log("Try Send")
-        ipcRendererApi.send('send_test_message', 'Hello from renderer process!')
+        if (ipcRendererApi.send('send_test_message', 'Hello from renderer process!'))
+        {
+            this.waiting = true
+        }
     },
 
     mounted()
     {
-        ipcRendererApi.on('reply_test_message', function (args){
-            this.dialog = false
+        ipcRendererApi.on('reply_test_message', function (args) {
+
+            // setTimeout(() => (this.waiting = false), 1000)
 
             console.log("Reply: ", args)
-        })
+        }.bind(this))
     }
 }
 
